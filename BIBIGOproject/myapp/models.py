b465f08a015dbf12d9289624b63c29c2f66b1a4b
xuejxup <xuejxup@gmail.com>
2025-02-26 22:01:31 +0800

    models.py文件更新

--- BIBIGOproject/myapp/models.py
+++ BIBIGOproject/myapp/models.py
@@ -52,44 +52,21 @@ class Login(models.Model):
 
 
 
-class Store(models.Model):
-    id = models.AutoField(primary_key=True)  # 自動遞增的主鍵
-    name = models.CharField(max_length=255)  # 商店名稱，不允許為空
+# Create your models here.
+class Dreamreal(models.Model):
+    website = models.CharField(max_length=50)
+    mail = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    phonenumber = models.IntegerField()
+    #online = models.ForeignKey('Online', default=1, on_delete=models.CASCADE, db_constraint=False)
 
-    def __str__(self):
-        return self.name  # 在 Django Admin 或 shell 顯示商店名稱
+    class Meta:
+        db_table = "dreamreal"
+        
+# class Online(models.Model):
+#     domain = models.CharField(max_length=30)
 
-
-class Category(models.Model):
-    id = models.AutoField(primary_key=True)  # 自動遞增的主鍵
-    store = models.ForeignKey(Store, on_delete=models.CASCADE)  # 關聯到 `store` 表
-    name = models.CharField(max_length=255)  # 分類名稱，不允許為空
-
-    def __str__(self):
-        return self.name  # 在 Django Admin 或 shell 顯示分類名稱
-
-
-class Product(models.Model):
-    id = models.AutoField(primary_key=True)  # 自動遞增的主鍵
-    store = models.ForeignKey(Store, on_delete=models.CASCADE)  # 關聯 store 表
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # 關聯 category 表
-
-    def __str__(self):
-        return f"Product {self.id}"  # 讓 Django Admin 介面顯示 Product ID
-
-
-class ProductDetail(models.Model):
-    id = models.AutoField(primary_key=True)  # 自動遞增的主鍵
-    product = models.ForeignKey(  # 外鍵關聯到 `product` 表的 `id`
-        Product,
-        on_delete=models.CASCADE
-    )
-    name = models.CharField(max_length=255)  # 商品名稱
-    price = models.DecimalField(max_digits=10, decimal_places=2)  # 商品價格
-    image_url = models.TextField(blank=True, null=True)  # 圖片網址，可為空
-    product_url = models.TextField(blank=True, null=True)  # 產品頁面網址，可為空
-
-    def __str__(self):
-        return self.name  # 在 Django Admin 或 shell 顯示商品名稱
+#     class Meta:
+#         db_table = "online"
 
 
