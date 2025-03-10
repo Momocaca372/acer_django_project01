a58e658cdffa8b7bc61d1833b20d0e647bb23c30
aaa58102003 <felix58102003@gmail.com>
2025-03-11 01:29:20 +0800

    重新分類

--- BIBIGOproject/myapp/models.py
+++ BIBIGOproject/myapp/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     product_url = models.URLField(max_length=500)
-    value = models.IntegerField(null=True, blank=True)
+    value = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.name
